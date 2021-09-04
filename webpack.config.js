050164c85d5c097d5307034d3ad682b4e36869e4
tjkendev <jake-basu@hotmail.co.jp>
2021-09-04 22:34:59 +0900

    update webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,9 @@ module.exports = {
   // module
   module: {
     rules: [
-      {test: /\.jade$/, loader: "raw-loader!pug-plain-loader"},
-      {test: /\.less$/, loader: 'style!css!less'},
-      {test: /\.coffee$/, loader: 'coffee-loader'},
+      {test: /\.jade$/, use: ["raw-loader", "pug-plain-loader"]},
+      {test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"]},
+      {test: /\.coffee$/, use: ["coffee-loader"]},
     ],
   },
 
